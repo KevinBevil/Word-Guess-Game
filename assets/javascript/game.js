8d0bea17fd6fa75333e992514241c0b418f5988c
Kevin Bevil <kbevil@gmail.com>
2019-03-13 09:23:17 -0400

    Much JS added...Game semi functioning now, but correct answer will not hold on to the correct guessed letter.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,34 +14,58 @@ var gameState = {
       return someArray[(Math.floor(Math.random() * someArray.length))];
    }
 }
+// replaces index of answer string with the correct guess
+function replaceIndexWithCorrectGuess(someString, index, x) {
+   var spacesStrAftr = '';
+   var spacesStrBfr = '';
+   for (var i = 0; i < index; i++) {
+      spacesStrBfr += "_ ";
+   }
+   for (var i = index; i < someString.length; i++) {
+      spacesStrAftr += ' _'
+   }
+   var stringWithAddedLetter = spacesStrBfr + x + spacesStrAftr;
+   return stringWithAddedLetter;
+}
 
-var wordsToSpaces = function (someString) {
-   // function that creates same number of spaces as letters in chosen word
+
+// function that creates same number of spaces as letters in chosen word
+function wordsToSpaces(someString) {
    var strOfSpaces = '';
-   for (var i = 0; i < someString.length; i++){
+   for (var i = 0; i < someString.length; i++) {
       strOfSpaces = strOfSpaces + '_ ';
    }
    return strOfSpaces;
 }
 // choose the word for the current gameplay
 var wordChosen = gameState.playNewWord(instrumentArray);
-
+var stringOfRejectedLetters = '';
 // setting up the variables to manipulate the on-screen html text
 
-var wordElem = document.getElementById('blanks-to-word').innerHTML;
+var wordElem = document.getElementById('blanks-to-word');
 var winsElem = document.getElementById('wins');
 var lossElem = document.getElementById('losses');
 var guessedLettersElem = document.getElementById('guessed-letters');
 var guessesLeftElem = document.getElementById('num-of-guesses-left');
 
-// console.log(wordsToSpaces(wordChosen));
-// console.log(wordChosen);
+console.log(wordsToSpaces(wordChosen));
+console.log(wordChosen);
 
 wordElem.textContent = wordsToSpaces(wordChosen);
 
 
 document.onkeyup = function (event) {
-   var userKey = event.key;
+   var userGuess = event.key;
+   // if the character guessed is contained in the string
+   if (wordChosen.includes(userGuess)) {
+      var correctGuessIndex = wordChosen.indexOf(userGuess);
+      var updatedAnswer = replaceIndexWithCorrectGuess(wordChosen, correctGuessIndex, userGuess);
+      wordElem.textContent = updatedAnswer;
+   } else {
+      stringOfRejectedLetters = stringOfRejectedLetters + userGuess;
+      guessedLettersElem.textContent = stringOfRejectedLetters;
+   }
+
 
 
 
