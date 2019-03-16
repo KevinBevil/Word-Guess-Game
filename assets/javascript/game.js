3f067a6c1f82629ddcbba99c388a4d02a7ce43a6
Kevin Bevil <kbevil@gmail.com>
2019-03-16 16:52:13 -0400

    Letter guessing actually working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,52 @@
-// keeps track of number of wins
-var winCounter;
-// keeps track of number of losses
-var lossCounter;
-// array to hold all the names of the orchestral instruments
-var instrumentArray = ['violin', 'viola', 'cello', 'bass', 'flute', 'piccolo', 'oboe', 'english horn', 'clarinet', 'bass', 'clarinet', 'bassoon', 'contrabassoon', 'saxophone', 'trumpet', 'french horn', 'trombone', 'tuba', 'celesta', 'piano', 'harpsichord', 'organ', 'synthesizer', 'harp', 'timpani', 'snare drum', 'bass drum', 'cymbals', 'tambourine', 'triangle', 'xylophone', 'glockenspiel', 'chimes', 'marimba', 'vibraphone'];
+// // keeps track of number of wins
+// var winCounter;
+// // keeps track of number of losses
+// var lossCounter;
+// // array to hold all the names of the orchestral instruments
+// var instrumentArray = ['violin', 'viola', 'cello', 'bass', 'flute', 'piccolo', 'oboe', 'english horn', 'clarinet', 'bass', 'clarinet', 'bassoon', 'contrabassoon', 'saxophone', 'trumpet', 'french horn', 'trombone', 'tuba', 'celesta', 'piano', 'harpsichord', 'organ', 'synthesizer', 'harp', 'timpani', 'snare drum', 'bass drum', 'cymbals', 'tambourine', 'triangle', 'xylophone', 'glockenspiel', 'chimes', 'marimba', 'vibraphone'];
 
 // object containing properties and method of the game
-var gameState = {
-   winCounter: 0,
-   lossCounter: 0,
+// var gameState = {
+//    winCounter: 0,
+//    lossCounter: 0,
 
-   playNewWord: function (someArray) {
-      return someArray[(Math.floor(Math.random() * someArray.length))];
-   }
-}
+//    playNewWord: function (someArray) {
+//       return someArray[(Math.floor(Math.random() * someArray.length))];
+//    }
+// }
 // replaces index of answer string with the correct guess
-function replaceIndexWithCorrectGuess(someString, index, x) {
-   var spacesStrAftr = '';
-   var spacesStrBfr = '';
-   for (var i = 0; i < index; i++) {
-      spacesStrBfr += "_ ";
-   }
-   for (var i = index; i < someString.length; i++) {
-      spacesStrAftr += ' _'
-   }
-   var stringWithAddedLetter = spacesStrBfr + x + spacesStrAftr;
-   return stringWithAddedLetter;
-}
+// function replaceIndexWithCorrectGuess(someString, index, x) {
+//    var spacesStrAftr = '';
+//    var spacesStrBfr = '';
+//    for (var i = 0; i < index; i++) {
+//       spacesStrBfr += "_ ";
+//    }
+//    for (var i = index; i < someString.length; i++) {
+//       spacesStrAftr += ' _'
+//    }
+//    var stringWithAddedLetter = spacesStrBfr + x + spacesStrAftr;
+//    return stringWithAddedLetter;
+// }
 
-function validate(strValue) {
-   var objRegExp = /^[a-z]+$/;
-   return objRegExp.test(strValue);
-}
+// function validate(strValue) {
+//    var objRegExp = /^[a-z]+$/;
+//    return objRegExp.test(strValue);
+// }
 
 // function that creates same number of spaces as letters in chosen word
-function wordsToSpaces(someString) {
-   var strOfSpaces = '';
-   for (var i = 0; i < someString.length; i++) {
-      strOfSpaces = strOfSpaces + '_ ';
-   }
-   return strOfSpaces;
-}
+// function wordsToSpaces(someString) {
+//    var strOfSpaces = '';
+//    for (var i = 0; i < someString.length; i++) {
+//       strOfSpaces = strOfSpaces + '_ ';
+//    }
+//    return strOfSpaces;
+// }
 // choose the word for the current gameplay
-var wordChosen = gameState.playNewWord(instrumentArray);
+// var wordChosen = gameState.playNewWord(instrumentArray);
 var stringOfRejectedLetters = '';
 // setting up the variables to manipulate the on-screen html text
 
-var wordElem = document.getElementById('blanks-to-word');
+var secretWordElem = document.getElementById('secret-word');
 var winsElem = document.getElementById('wins');
 var lossElem = document.getElementById('losses');
 var guessedLettersElem = document.getElementById('guessed-letters');
---
+++ assets/javascript/game2.js
@@ -0,0 +1,76 @@
+// keeps track of number of wins
+var winCounter;
+// keeps track of number of losses
+var lossCounter;
+// array to hold all the names of the orchestral instruments
+var instrumentArray = ['violin', 'viola', 'cello', 'bass', 'flute', 'piccolo',
+'oboe', 'english horn', 'clarinet', 'bass', 'clarinet', 'bassoon',
+'contrabassoon', 'saxophone', 'trumpet', 'french horn', 'trombone', 'tuba',
+'celesta', 'piano', 'harpsichord', 'organ', 'synthesizer', 'harp', 'timpani',
+'snare drum', 'bass drum', 'cymbals', 'tambourine', 'triangle', 'xylophone',
+ 'glockenspiel', 'chimes', 'marimba', 'vibraphone'];
+
+// simple wins and losses counters
+var winCounter = 0;
+var lossCounter = 0;
+
+// function to choose random word from instrumentArray
+function playNewWord(someArray) {
+   return someArray[(Math.floor(Math.random() * someArray.length))];
+}
+
+// validates that user's guess is alpha
+function validate(strValue) {
+   var objRegExp = /^[a-z]+$/;
+   return objRegExp.test(strValue);
+}
+
+// secret word that the player is trying to discover
+var secretWord = playNewWord(instrumentArray);
+// generates a string of underscores separated by spaces equal to number of
+// characters in
+// secretWord
+var secretWordToUnderscores = Array(secretWord.length).fill("_").join(" ");
+// arr_secretWord is array where each element is a character of secretWord
+var arr_secretWord = secretWord.split("");
+// arr_secretWordToUnderscores is array where each element is an underscore
+var arr_secretWordToUnderscores = secretWordToUnderscores.split(" ");
+
+var stringOfRejectedLetters = '';
+
+// console.log("secretWord: " + secretWord + "secretWordToUnderscores: " +
+// secretWordToUnderscores + "arr_secretWord: " + arr_secretWord +
+// "arr_secretWordToUnderscores: " + arr_secretWordToUnderscores);
+
+// hooks for wins, losses, secret word, number of guesses left, and letters
+// already guessed
+var winsElem = document.getElementById('wins');
+var lossElem = document.getElementById('losses');
+var secretWordElem = document.getElementById('secret-word');
+var guessedLettersElem = document.getElementById('guessed-letters');
+var guessesLeftElem = document.getElementById('num-of-guesses-left');
+
+function switchElemsAtGivenIndex(wordArray, underscoreArray, index) {
+   underscoreArray[index] = wordArray[index];
+   wordArray[index] = '_';
+}
+
+
+document.onkeyup = function (event) {
+   var gameWon = false;
+   // variable to hold user's alpha guess
+   var userGuess = event.key;
+   if (validate(userGuess)) {
+      while (arr_secretWord.indexOf(userGuess) !== -1) {
+         var index = arr_secretWord.indexOf(userGuess);
+         arr_secretWordToUnderscores[index] = userGuess;
+         arr_secretWord[index] = '_';
+      }
+   }
+   if (validate(userGuess)){
+      stringOfRejectedLetters = stringOfRejectedLetters + ' ' + userGuess;
+      guessedLettersElem.textContent = stringOfRejectedLetters;
+   }
+   secretWordElem.textContent = arr_secretWordToUnderscores.join(" ");
+   
+}
